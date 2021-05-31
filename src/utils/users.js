a6afa7fc3e5c395e0cdcd299436c764479da223e
leahzeisner <zeisner.l@northeastern.edu>
2021-05-31 15:08:13 -0400

    Initial commit

---
+++ src/utils/users.js
@@ -0,0 +1,53 @@
+const users = []
+
+
+
+const addUser = ({ id, username, room }) => {
+    if (!username || !room) {
+        return {
+            error: 'Error! Username and room are required.'
+        }
+    }
+
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    if (existingUser) {
+        return {
+            error: 'Error! Username is already in use.'
+        }
+    }
+
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+
+
+const getUser = (id) => users.find((user) => user.id === id)
+
+
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom }
